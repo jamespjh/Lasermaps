6f2968c93c3a5e65d57652e87df539d123a9ce47
James Hetherington <j.hetherington@ucl.ac.uk>
2013-04-24 21:11:32 +0100

    Typo in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='websequence',
+setup(name='lasermaps',
       version='0.01',
       description='Create map image suitable for laser cutting from gmaps',
       author='James Hetherington',
